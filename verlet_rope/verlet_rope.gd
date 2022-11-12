e244cf7e2a1ad18347c087061ce49bd904857e67
Renato Rotenerg <renato.rotenberg@gmail.com>
2022-11-12 19:53:14 -0300

    Implement setters for VerletRope

--- verlet_rope/verlet_rope.gd
+++ verlet_rope/verlet_rope.gd
@@ -2,29 +2,27 @@ class_name VerletRope
 extends Line2D
 
 
-@export var lenght := 500.0
-@export var number_of_segments := 50
+@export var lenght := 500.0:
+	set(value):
+		lenght = value
+		_update_segment_lenght()
+@export var number_of_segments := 50:
+	set(value):
+		number_of_segments = value
+		_update_segment_lenght()
+		_reset()
 @export var constraint_iterations := 10
 @export var gravity := Vector2(0.0, 10.0)
 
-var _current_points: PackedVector2Array
-var _old_points: PackedVector2Array
+var _current_points := PackedVector2Array()
+var _old_points := PackedVector2Array()
 
 var _segment_lenght: float
 
 
 func _ready() -> void:
-	_segment_lenght = lenght / float(number_of_segments)
-
-	clear_points()
-	for i in range(number_of_segments + 1):
-		add_point(Vector2(0, _segment_lenght * i))
-
-	_current_points = PackedVector2Array()
-	_current_points.resize(get_point_count())
-	for i in range(_current_points.size()):
-		_current_points.set(i, to_global(points[i]))
-	_old_points = _current_points.duplicate()
+	_update_segment_lenght()
+	_reset()
 
 
 func _process(delta: float) -> void:
@@ -32,6 +30,19 @@ func _process(delta: float) -> void:
 	for _i in range(constraint_iterations):
 		_constrain()
 	_commit()
+
+
+func _reset() -> void:
+	clear_points()
+	for i in range(number_of_segments + 1):
+		add_point(Vector2(0, _segment_lenght * i))
+
+	_current_points.resize(get_point_count())
+	for i in range(_current_points.size()):
+		_current_points.set(i, to_global(points[i]))
+	
+	_old_points.clear()
+	_old_points.append_array(_current_points)
 
 
 func _simulate(delta: float) -> void:
@@ -68,9 +79,9 @@ func _constrain_start() -> void:
 	var first_point := _current_points[0]
 	var second_point := _current_points[1]
 
-	var change_direction := _get_change_direction(first_point, second_point)
+	var change := _calculate_change_vector(first_point, second_point)
 
-	_current_points.set(1, second_point + change_direction)
+	_current_points.set(1, second_point + change)
 
 
 func _constrain_middle() -> void:
@@ -78,13 +89,13 @@ func _constrain_middle() -> void:
 		var first_point := _current_points[i]
 		var second_point := _current_points[i + 1]
 
-		var change_direction := _get_change_direction(first_point, second_point)
+		var change := _calculate_change_vector(first_point, second_point)
 
-		_current_points.set(i, first_point - change_direction * 0.5)
-		_current_points.set(i + 1, second_point + change_direction * 0.5)
+		_current_points.set(i, first_point - change * 0.5)
+		_current_points.set(i + 1, second_point + change * 0.5)
 
 
-func _get_change_direction(first_point: Vector2, second_point: Vector2) -> Vector2:
+func _calculate_change_vector(first_point: Vector2, second_point: Vector2) -> Vector2:
 	var distance := (first_point - second_point).length()
 	var error := absf(distance - _segment_lenght)
 
@@ -93,6 +104,9 @@ func _get_change_direction(first_point: Vector2, second_point: Vector2) -> Vecto
 		change_direction = (first_point - second_point).normalized()
 	else:
 		change_direction = (second_point - first_point).normalized()
-	change_direction *= error
 
-	return change_direction
+	return change_direction * error
+
+
+func _update_segment_lenght() -> void:
+	_segment_lenght = lenght / float(number_of_segments)
